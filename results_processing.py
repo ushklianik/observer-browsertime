9bf85b64a55d76155c2bc1ad19073c0a28c781fe
Mykhailo_Hunko <mykhailo.hunko@gmail.com>
2022-11-29 17:07:39 +0200

    Add settings from quality gate integration

--- results_processing.py
+++ results_processing.py
@@ -163,6 +163,16 @@ try:
                     "test_id": sys.argv[1],
                     "report_id": REPORT_ID
                 }
+                if integrations.get("processing") and "quality_gate" in integrations["processing"].keys():
+                    quality_gate_config = integrations['processing']['quality_gate']
+                else:
+                    quality_gate_config = {}
+                if quality_gate_config.get('check_performance_degradation') and \
+                        quality_gate_config['check_performance_degradation'] != -1:
+                    event["performance_degradation_rate"] = quality_gate_config['performance_degradation_rate']
+                if quality_gate_config.get('check_missed_thresholds') and \
+                        quality_gate_config['check_missed_thresholds'] != -1:
+                    event["missed_thresholds"] = quality_gate_config['missed_thresholds_rate']
 
                 res = requests.post(task_url, json=event, headers={'Authorization': f'bearer {TOKEN}',
                                                                    'Content-type': 'application/json'})
